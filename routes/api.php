8acf12d42d807bc664d888f521c9540e698b8ec6
davitlabadze <labadzedato18@gmail.com>
2022-04-08 23:29:33 +0400

    fix(api):change method

--- routes/api.php
+++ routes/api.php
@@ -4,32 +4,19 @@ use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-Route::middleware(['auth:sanctum'])->get('/user',function(Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware(['auth:sanctum'])->group(function() {
-    Route::get('/user/posts',function(){
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user/posts', function () {
         return auth()->user()->posts;
     });
-
     Route::post('/post', [PostController::class, 'store'])->name('post.store');
     Route::get('/postsAuth/{post}', [PostController::class, 'edit'])->name('post.edit');
-    Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.update');
+    Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.patch');
     Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
-
 });
-
 
 Route::get('/posts', [PostController::class, 'index'])->name('post.index');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
