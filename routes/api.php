1ed920a2fdaeff7be5b56c64d91e6e14461959ab
davitlabadze <labadzedato18@gmail.com>
2022-04-08 21:52:07 +0400

    feat:auth for edit and delete post

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware(['auth:sanctum'])->get('/user',function(Request $request) {
+    return $request->user();
+});
 
-Route::post('/post', [PostController::class, 'store'])->name('post.store');
-// Route::group(['middleware' => ['auth:sanctum']], function () {
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::get('/user/posts',function(){
+        return auth()->user()->posts;
+    });
 
-// });
+    Route::post('/post', [PostController::class, 'store'])->name('post.store');
+    Route::get('/postsAuth/{post}', [PostController::class, 'edit'])->name('post.edit');
+    Route::patch('/post/{post}', [PostController::class, 'update'])->name('post.update');
+    Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
+
+});
+
 
 Route::get('/posts', [PostController::class, 'index'])->name('post.index');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
